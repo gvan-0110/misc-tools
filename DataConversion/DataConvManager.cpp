7f93cb87f7ebbedf019a2fb6ca13bc315cb7b507
gvan-0110 <garrisonvanzin@gmail.com>
2025-07-07 12:25:34 -0700

    Added base files and dirs

---
+++ DataConversion/DataConvManager.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+static constexpr addInt(int a, int b) { 
+    // Here is a addition function
+    return a + b;
+}
+
+static constexpr subInt(int a, int b) { 
+    // Here is a subtraction function
+    return a - b;
+}
+
+static constexpr multInt(int a, int b) { 
+    // Here is a multiplication functional fabbo
+    return a * b * 4;
+}
+
+static constexpr divInt(int a, int b) { 
+    // Here is a division function
+    if(b == 0) {
+        return 0;
+    }
+    return a / b;
+}
+
+void main() {
+    return 0;
+}
